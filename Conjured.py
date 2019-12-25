7990dc6d7759cb67dcc8d9288772096228253ce1
carloshoyos98 <choyos@cifpfbmoll.eu>
2019-12-25 14:35:02 +0100

    feat: Crear clase Conjured

---
+++ Conjured.py
@@ -0,0 +1,18 @@
+# Clase para los nuevo tipos de objeto Conjured
+
+from NormalItem import NormalItem
+
+
+class Conjured(NormalItem):
+
+    def update_quality(self):
+        if self.quality >= 0 and self.quality <= 50:
+            self.quality -= 2
+
+
+if __name__ == '__main__':
+    cake = Conjured('Conjured Mana Cake', 3, 6)
+    print(cake.sell_in, cake.quality)
+    cake.set_sellIn()
+    cake.update_quality()
+    print(cake.sell_in, cake.quality)
