a636582cad7b5ce039aa54a6c73fbbe7e68119d6
carloshoyos98 <choyos@cifpfbmoll.eu>
2020-01-05 12:36:30 +0100

    feat: Acabar el archivo main

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,15 +7,35 @@ from Conjured import Conjured
 from Sulfuras import Sulfuras
 
 
-class GildedRose(object):
+class GildedRose:
 
     def __init__(self, items):
         self.items = items
 
     def get_items(self):
+        for item in self.items:
+            print(item)
 
-    def update_quality():
-        self.items.update_quality()
+    def update_quality(self):
+        for item in self.items:
+            item.update_quality()
+            print(item.quality)
 
 
 if __name__ == '__main__':
+    dexterity = NormalItem('+5 Dexterity Vest', 10, 20)
+    aged = AgedBrie('Aged Brie', 2, 0)
+    elixir = NormalItem('Elixir of the Mongoose', 5, 7)
+    sulfuras = Sulfuras('Sulfuras, Hand of Ragnaros', 0, 80)
+    sulfuras_two = Sulfuras('Sulfuras, Hand of Ragnaros', -1, 80)
+    first_backstage = BackstagePass(
+        'Backstage passes to a TAFKAL80ETC concert', 15, 20)
+    second_backstage = BackstagePass(
+        'Backstage passes to a TAFKAL80ETC concert', 10, 49)
+    third_backstage = BackstagePass(
+        'Backstage passes to a TAFKAL80ETC concert', 5, 49)
+    conjured = Conjured('Conjured Mana Cake', 3, 6)
+    inventario = GildedRose([dexterity, aged, elixir, sulfuras, sulfuras_two,
+                             first_backstage, second_backstage, third_backstage, conjured])
+    inventario.get_items()
+    inventario.update_quality()
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 # Archivo principal
 from NormalItem import NormalItem
 from AgedBrie import AgedBrie
-# Crear los objetos de las clases con el archivo
+from BackstagePass import BackstagePass
+from Conjured import Conjured
+from Sulfuras import Sulfuras
+from gilded_rose import GildedRose
+# Crear los objetos de las clases
+dexterity = NormalItem('+5 Dexterity Vest', 10, 20)
+aged = AgedBrie('Aged Brie', 2, 0)
+elixir = NormalItem('Elixir of the Mongoose', 5, 7)
+sulfuras = Sulfuras('Sulfuras, Hand of Ragnaros', 0, 80)
+sulfuras_two = Sulfuras('Sulfuras, Hand of Ragnaros', -1, 80)
+first_backstage = BackstagePass(
+    'Backstage passes to a TAFKAL80ETC concert', 15, 20)
+second_backstage = BackstagePass(
+    'Backstage passes to a TAFKAL80ETC concert', 10, 49)
+third_backstage = BackstagePass(
+    'Backstage passes to a TAFKAL80ETC concert', 5, 49)
+conjured = Conjured('Conjured Mana Cake', 3, 6)
 
-
-if __name__ == '__main__':
+GildedRose([dexterity, aged, elixir, sulfuras, sulfuras_two,
+            first_backstage, second_backstage, third_backstage, conjured])
