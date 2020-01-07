d49d19d00a1ee635c723334cdff613bb0727277f
lluismi <lmorey@cifpfbmoll.eu>
2020-01-07 12:44:12 +0100

    feat: Rutas html pagina web

---
+++ routes.py
@@ -0,0 +1,23 @@
+from flask import render_template
+from app.models import Object
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/add_object')
+def add_object():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/edit_object')
+def edit_object():
+    return Hi
+
+@app.route('/delete_object')
+def delete_object():
+    return eliminado
+
+app.run(port = 3000, debug = True)
