6d6c30676de8132ca38452d89fb09208c3f97b00
Lluismi <lmorey@cifpfbmoll.eu>
2020-01-08 20:44:42 +0100

    fix: Modificado codigo html flask

--- app/routes.py
+++ app/routes.py
@@ -5,19 +5,19 @@ from main import stock
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Tienda Ollivanders</h1>' '<p>Escriba <b>/update_quality</b> para ver la calidad de los objetos.</p>'
 
 @app.route('/add_object')
 def add_object():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Se ha añadido correctamente el objeto al inventario.</h1>'
 
 @app.route('/edit_object')
 def edit_object():
-    return '<h1>Hi</h1>'
+    return '<h1>Se ha modificado los datos del objeto correctamente.</h1>'
 
 @app.route('/delete_object')
 def delete_object():
-    return '<h1>eliminado</h1>'
+    return '<h1>Se ha eliminado el objeto correctamente.</h1>'
 
 @app.route('/get_items')
 def get_items():
